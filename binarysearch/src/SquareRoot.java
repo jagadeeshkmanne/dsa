2e927269c1c5ea16f42358dc05b099d30e8a3426
jagadeesh manne <jagadeesh.k.manne@gmail.com>
2022-07-19 15:55:32 +0530

    Square Root of a number using binary search

--- binarysearch/src/SquareRoot.java
+++ binarysearch/src/SquareRoot.java
@@ -2,24 +2,18 @@ public class SquareRoot {
 
     //problem: https://leetcode.com/problems/valid-perfect-square/
     //solution: https://leetcode.com/submissions/detail/749854269/
-
-    public static void main(String args[]) {
-        System.out.println(mySqrt(49));
-        System.out.println(mySqrt(3));
-    }
-
-    public static int mySqrt(int number) {
-        if(number == 0 || number == 1)
+    public int mySqrt(int number) {
+        if (number == 0 || number == 1)
             return number;
 
         int start = 0;
         int end = number;
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            if (mid == number/mid) {
+            if (mid == number / mid) {
                 return mid;
-            } else if (mid > number/mid) {
-                end =  mid - 1;
+            } else if (mid > number / mid) {
+                end = mid - 1;
             } else {
                 start = mid + 1;
             }
